b1e501ce9f10b3dc66f89b5dac0d1db725679e1d
Neil Winton <nwinton@pivotal.io>
2021-04-08 12:17:50 +0100

    Exercise 1b: refactoring

--- src/main/kotlin/uk/org/winton/learning/gorman/ch04/RomanNumerals.kt
+++ src/main/kotlin/uk/org/winton/learning/gorman/ch04/RomanNumerals.kt
@@ -2,16 +2,17 @@ package uk.org.winton.learning.gorman.ch04
 
 fun Int.toRoman(): String {
     val thousands = (this / 1000).romanUnits("m", "?", "?")
-    val hundreds = ((this % 1000) / 100).romanUnits("c", "d", "m")
-    val tens = ((this % 100) / 10).romanUnits("x", "l", "c")
+    val hundreds = (this / 100).romanUnits("c", "d", "m")
+    val tens = (this / 10).romanUnits("x", "l", "c")
     return thousands + hundreds + tens + (this % 10).romanUnits("i", "v", "x")
 }
 
 private fun Int.romanUnits(onesLetter: String, fivesLetter: String, tensLetter: String): String {
-    val (prefix, suffix) = if (this % 5 == 4) Pair(onesLetter, "") else Pair("", onesLetter.repeat(this % 5))
+    val units = this % 10
+    val (prefix, suffix) = if (units % 5 == 4) Pair(onesLetter, "") else Pair("", onesLetter.repeat(units % 5))
     val middle = when {
-        this > 8 -> tensLetter
-        this > 3 -> fivesLetter
+        units == 9 -> tensLetter
+        units >= 4 -> fivesLetter
         else -> ""
     }
     return prefix + middle + suffix
